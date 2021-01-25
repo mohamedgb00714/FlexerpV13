446d26288203e7265ad9290bea8cf8d45c6e129c
msaid mohamed el hadi <mohamedgb00714@gmail.com>
2021-01-25 21:50:13 +0100

    test

---
+++ mgb_tree_months/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    'name': 'Web Gantt 3months',
+    'category': 'Hidden',
+    'description': """
+Odoo Web Gantt chart view.
+=============================
+
+""",
+    'version': '2.0',
+    'depends': ['web'],
+    'data' : [
+
+    ],
+    'qweb': [
+
+    ],
+    'auto_install': True,
+    'license': 'OEEL-1',
+}
